e74b0a5f831be4d0f60edf8dfe0a2e58d6e6dc3c
JackDemps <anpankid@gmail.com>
2023-02-08 15:50:45 -0500

    Able to change background on icon click

--- hypeTabs.js
+++ hypeTabs.js
@@ -1,16 +1,27 @@
-document.body.style.border = "5px solid red";
+// document.body.style.border = "5px solid green";
+// let css = "body { border: 20px dotted pink; }";
+
+browser.browserAction.onClicked.addListener(changeTabColor);
+// browser.tabs.onActivated.addListener(changeTabColor);
+
 // in background.js
-browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    browser.tabs.onCreated.addListener(changeTabColor("red"));
-});
-  
-function changeTabColor(color) {
-    browser.tabs.query({currentWindow: true})
-      .then((tabs) => {
-        const activeTab = tabs[0];
-        browser.tabs.executeScript({
-          code: `document.body.style.backgroundColor = "${color}"`
-        });
+// browser.tabs.query({ currentWindow: true }, (tabs) => {
+//   var activeTab = tabs[0];
+//   tabs.forEach((tab) => {
+//     console.log('hello');
+//     // browser.tabs.insertCSS({ code: css }).then(null, console.log('Error'));
+//   })
+
+//   browser.tabs.onActivated.addListener(changeTabColor("red"));
+// });
+
+function changeTabColor(e) {
+  // document.body.style.border = "5px solid red";
+  browser.tabs.query({ currentWindow: true, active: true })
+    .then((tabs) => {
+      const activeTab = tabs[0].id;
+      browser.tabs.executeScript(activeTab, {
+        code: 'document.body.style.backgroundColor = "green"'
+      });
     });
 }
