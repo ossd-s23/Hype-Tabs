5a145e4f1518df25916f3bbafcc2ae0505d73350
Jiajun-Jiao <jiaojiajunqd@gmail.com>
2023-02-08 17:31:20 -0500

    updated format

--- hypeTabs.js
+++ hypeTabs.js
@@ -1,15 +1,3 @@
-browser.browserAction.onClicked.addListener(changeTabColor);
-
-function changeTabColor() {
-    browser.tabs.query({active:true, currentWindow: true})
-      .then((tabs) => {
-        const activeTab = tabs[0].id;
-        browser.tabs.executeScript(activeTab, {
-            code: `document.body.style.backgroundColor = "green"`
-          });
-      });
-
-  }
 browser.tabs.onActivated.addListener(toGoogle);
 function toGoogle() {
     browser.tabs.query({active:true, currentWindow: true})
@@ -17,8 +5,29 @@ function toGoogle() {
         const activeTab = tabs[0].id;
         browser.tabs.update(
             activeTab,
-            {url: "https://www.google.com"}
+            {
+                url: "https://www.google.com",
+                active: true
+            }
         )
-      });
+    });
 
   }
+
+browser.browserAction.onClick.addListener(changeTabColor);
+function changeTabColor() {
+    browser.tabs.query({active:true, currentWindow: true})
+      .then((tabs) => {
+        const activeTab = tabs[0].id;
+        browser.tabs.executeScript(activeTab, {
+            code: `document.body.style.backgroundColor = "green"`
+        });
+        // browser.browserAction.setIcon({
+        //     tabId: activeTab,
+        //     48: "icons/hype-tabs-48.png",
+        // })
+        // browser.browserAction.setTitle({
+        //     title: "COOL"
+        // })
+    });
+}
