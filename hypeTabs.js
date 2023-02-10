4368382ff3cdb4c3783ab20ae8f043138766b7a3
JackDemps <anpankid@gmail.com>
2023-02-09 20:13:49 -0500

    Theme changing functionality

--- hypeTabs.js
+++ hypeTabs.js
@@ -1,27 +1,36 @@
-// document.body.style.border = "5px solid green";
-// let css = "body { border: 20px dotted pink; }";
-
 browser.browserAction.onClicked.addListener(changeTabColor);
-// browser.tabs.onActivated.addListener(changeTabColor);
+function changeTabColor(activeInfo) {
+    browser.tabs.query({ currentWindow: true })
+        .then((tabs) => {
+            const pngURL = browser.runtime.getURL("icons/hype-tabs-48.png");
+            tabs.forEach((tab) => {
+                const activeTab = tab.id;
+                browser.tabs.executeScript(activeTab, {
+                    code: `
+                        document.body.style.backgroundColor = "green";
+                        document.title = "HYPE";
+                        document.querySelectorAll("link[rel*='icon']").forEach((element) => {
+                            element.href = "${pngURL}";
+                        });
+                    `
+                });
+            });
+        });
+}
 
-// in background.js
-// browser.tabs.query({ currentWindow: true }, (tabs) => {
-//   var activeTab = tabs[0];
-//   tabs.forEach((tab) => {
-//     console.log('hello');
-//     // browser.tabs.insertCSS({ code: css }).then(null, console.log('Error'));
-//   })
-
-//   browser.tabs.onActivated.addListener(changeTabColor("red"));
-// });
-
-function changeTabColor(e) {
-  // document.body.style.border = "5px solid red";
-  browser.tabs.query({ currentWindow: true, active: true })
-    .then((tabs) => {
-      const activeTab = tabs[0].id;
-      browser.tabs.executeScript(activeTab, {
-        code: 'document.body.style.backgroundColor = "green"'
-      });
-    });
+browser.tabs.onActivated.addListener(changeColor);
+function changeColor() {
+    const colors = ["red", "green", "blue", "yellow"];
+    const randColor = colors[Math.floor(Math.random() * colors.length)];
+    const theme = {
+        colors: {
+            toolbar: randColor,
+            tab_selected: randColor
+        }
+    };
+    browser.theme.update(theme);
+    // browser.tabs.query({ currentWindow: true })
+    //     .then((tabs) => {
+    //         browser.theme.update(theme);
+    //     });
 }
